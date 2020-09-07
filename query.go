5c64db53dbb315742faf0e8e2c810c54ab87492c
Jason Hancock <jason@jasonhancock.com>
2020-09-07 09:39:21 -0700

    more query options

--- query.go
+++ query.go
@@ -65,7 +65,60 @@ type QueryOption func(*options) error
 // WithSenderCallsign set the sender callsign in the query.
 func WithSenderCallsign(s string) QueryOption {
 	return func(o *options) error {
+		if _, ok := o.vals["receiverCallsign"]; ok {
+			return errCallsignExclusive
+		}
+		if _, ok := o.vals["callsign"]; ok {
+			return errCallsignExclusive
+		}
 		o.vals.Set("senderCallsign", s)
+		return nil
+	}
+}
+
+// WithReceiverCallsign set the receiver callsign in the query.
+func WithReceiverCallsign(s string) QueryOption {
+	return func(o *options) error {
+		if _, ok := o.vals["senderCallsign"]; ok {
+			return errCallsignExclusive
+		}
+		if _, ok := o.vals["callsign"]; ok {
+			return errCallsignExclusive
+		}
+		o.vals.Set("receiverCallsign", s)
+		return nil
+	}
+}
+
+var errCallsignExclusive = errors.New("only one of callsign, senderCallsign, or receiverCallsign can be specified at a time")
+
+// WithCallsign sets the Callsign of interest.
+func WithCallsign(s string) QueryOption {
+	return func(o *options) error {
+		if _, ok := o.vals["senderCallsign"]; ok {
+			return errCallsignExclusive
+		}
+		if _, ok := o.vals["receiverCallsign"]; ok {
+			return errCallsignExclusive
+		}
+
+		o.vals.Set("callsign", s)
+		return nil
+	}
+}
+
+// WithMode sets the mode of operation in the query.
+func WithMode(s string) QueryOption {
+	return func(o *options) error {
+		o.vals.Set("mode", s)
+		return nil
+	}
+}
+
+// WithReportLimit limits the number of records returned.
+func WithReportLimit(s int) QueryOption {
+	return func(o *options) error {
+		o.vals.Set("rptlimit", fmt.Sprintf("%d", s))
 		return nil
 	}
 }
@@ -99,6 +152,49 @@ func WithNoActive(i int) QueryOption {
 func WithAppContact(email string) QueryOption {
 	return func(o *options) error {
 		o.vals.Set("appcontact", email)
+		return nil
+	}
+}
+
+// WithRROnly will only return the reception reports if non-zero.
+func WithRROnly(i int) QueryOption {
+	return func(o *options) error {
+		o.vals.Set("rronly", fmt.Sprintf("%d", i))
+		return nil
+	}
+}
+
+// WithFrequencyRange sets a lower and upper bound for frequencies. Example: 14000000-14100000
+func WithFrequencyRange(lower, upper int) QueryOption {
+	return func(o *options) error {
+		if lower > upper {
+			return errors.New("lower frequency must be less than upper frequency")
+		}
+		o.vals.Set("frange", fmt.Sprintf("%d-%d", lower, upper))
+		return nil
+	}
+}
+
+// WithNoLocator will return reception reports without a locator if non-zero.
+func WithNoLocator(i int) QueryOption {
+	return func(o *options) error {
+		o.vals.Set("nolocator", fmt.Sprintf("%d", i))
+		return nil
+	}
+}
+
+// WithStatistics will return some statistics if non-zero.
+func WithStatistics(i int) QueryOption {
+	return func(o *options) error {
+		o.vals.Set("statistics", fmt.Sprintf("%d", i))
+		return nil
+	}
+}
+
+// WithLastSequenceNumber sets the last sequence number in the query.
+func WithLastSequenceNumber(s string) QueryOption {
+	return func(o *options) error {
+		o.vals.Set("lastseqno", s)
 		return nil
 	}
 }
